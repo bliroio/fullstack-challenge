00a423490841c1e2eb07d4080290b2d4b023f35c
Thoma997 <martin.thoma@web.de>
2025-03-12 16:36:13 +0100

    feat: add pagination to backend

--- server/src/routes/meetingRoutes.ts
+++ server/src/routes/meetingRoutes.ts
@@ -118,13 +118,38 @@ router.delete('/:id', meetingController.deleteMeeting);
  *     tags: [Meetings]
  *     responses:
  *       200:
- *         description: A list of meetings
+ *         description: A paginated list of meetings
  *         content:
  *           application/json:
  *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Meeting'
+ *               type: object
+ *               properties:
+ *                 docs:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Meeting'
+ *                 totalDocs:
+ *                   type: number
+ *                 limit:
+ *                   type: number
+ *                 hasPrevPage:
+ *                   type: boolean
+ *                 hasNextPage:
+ *                   type: boolean
+ *                 page:
+ *                   type: number
+ *                 totalPages:
+ *                   type: number
+ *                 offset:
+ *                   type: number
+ *                 prevPage:
+ *                   type: number
+ *                   nullable: true
+ *                 nextPage:
+ *                   type: number
+ *                   nullable: true
+ *                 pagingCounter:
+ *                   type: number
  *       500:
  *         description: Server error
  */
