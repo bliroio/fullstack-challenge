16abc63a6cb93fe8578751b94d09e46e59220b6c
talbizzz <a.talbi890@gmail.com>
2025-08-03 11:29:07 +0200

    add styled component, remove mui, add proper svg handling

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,20 +1,32 @@
 "use client";
 
 // Home.tsx
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import React from "react";
+import styled from "styled-components";
 import MeetingList from "./components/meetingList";
 
 const Home: React.FC = () => {
   return (
-    <Container maxWidth="md">
-      <Typography variant="h2" gutterBottom>
-        Meetings
-      </Typography>
+    <StyledContainer>
+      <StyledTitle>Meetings</StyledTitle>
       <MeetingList />
-    </Container>
+    </StyledContainer>
   );
 };
 
 export default Home;
+
+const StyledContainer = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 24px;
+`;
+
+const StyledTitle = styled.h2`
+  font-size: 3.75rem;
+  font-weight: 300;
+  line-height: 1.2;
+  letter-spacing: -0.00833em;
+  margin: 0 0 16px 0;
+  color: rgba(0, 0, 0, 0.87);
+`;
