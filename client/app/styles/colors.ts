ae9053b9b8d55d8dfdc18e12d603a027514052f7
talbizzz <a.talbi890@gmail.com>
2025-08-03 11:43:35 +0200

    add global styles

---
+++ client/app/styles/colors.ts
@@ -0,0 +1,33 @@
+type Colors = {
+  bliroOrange: string;
+  white: string;
+  dark1: string;
+  dark2: string;
+  dark3: string;
+  dark4: string;
+  dark5: string;
+  dark6: string;
+  dark7: string;
+  red: string;
+  green: string;
+  blue: string;
+  yellow: string;
+  flowkit: string;
+};
+
+export const colors: Colors = {
+  bliroOrange: "#F26835",
+  white: "#FFFFFF",
+  dark1: "#131a26",
+  dark2: "#2B313C",
+  dark3: "#424852",
+  dark4: "#71767d",
+  dark5: "#a1a3a8",
+  dark6: "#D0D1D4",
+  dark7: "#E7e8e9",
+  red: "#F43641",
+  green: "#00FF00",
+  blue: "#0000FF",
+  yellow: "#FFFF00",
+  flowkit: "#F5F5F5", // Example color for flowkit
+};
---
+++ client/app/styles/textStyles.ts
@@ -0,0 +1,242 @@
+// textStyles.js
+// Styled-components for all text styles from Figma
+
+import styled from "styled-components";
+
+// Headings
+export const H1 = styled.h1`
+  font-size: 48px;
+  line-height: 64px;
+  font-weight: 700;
+  margin: 0;
+`;
+
+export const H2 = styled.h2`
+  font-size: 40px;
+  line-height: 56px;
+  font-weight: 700;
+  margin: 0;
+`;
+
+export const H3 = styled.h3`
+  font-size: 36px;
+  line-height: 48px;
+  font-weight: 700;
+  margin: 0;
+`;
+
+export const H4 = styled.h4`
+  font-size: 32px;
+  line-height: 40px;
+  font-weight: 700;
+  margin: 0;
+`;
+
+// Subheadings (using <p> for flexibility)
+export const Subheading1 = styled.p`
+  font-size: 24px;
+  line-height: 36px;
+  font-weight: 400;
+  margin: 0;
+`;
+
+export const Subheading2 = styled.p`
+  font-size: 20px;
+  line-height: 30px;
+  font-weight: 400;
+  margin: 0;
+`;
+
+export const Subheading3 = styled.p`
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+  margin: 0;
+`;
+
+export const Subheading4 = styled.p`
+  font-size: 13px;
+  line-height: 18px;
+  font-weight: 400;
+  margin: 0;
+`;
+
+// Body Large
+export const BodyLargeRegular = styled.p`
+  font-size: 18px;
+  line-height: 26px;
+  font-weight: 400;
+  margin: 0;
+`;
+
+export const BodyLargeMedium = styled.p`
+  font-size: 18px;
+  line-height: 26px;
+  font-weight: 500;
+  margin: 0;
+`;
+
+export const BodyLargeSemiBold = styled.p`
+  font-size: 18px;
+  line-height: 26px;
+  font-weight: 600;
+  margin: 0;
+`;
+
+export const BodyLargeBold = styled.p`
+  font-size: 18px;
+  line-height: 26px;
+  font-weight: 700;
+  margin: 0;
+`;
+
+export const BodyLargeExtraBold = styled.p`
+  font-size: 18px;
+  line-height: 26px;
+  font-weight: 800;
+  margin: 0;
+`;
+
+// Body Normal
+export const BodyNormalRegular = styled.p`
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+  margin: 0;
+`;
+
+export const BodyNormalMedium = styled.p`
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 500;
+  margin: 0;
+`;
+
+export const BodyNormalSemiBold = styled.p`
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 600;
+  margin: 0;
+`;
+
+export const BodyNormalBold = styled.p`
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 700;
+  margin: 0;
+`;
+
+export const BodyNormalExtraBold = styled.p`
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 800;
+  margin: 0;
+`;
+
+// Body Small
+export const BodySmallRegular = styled.p`
+  font-size: 14px;
+  line-height: 22px;
+  font-weight: 400;
+  margin: 0;
+`;
+
+export const BodySmallMedium = styled.p`
+  font-size: 14px;
+  line-height: 22px;
+  font-weight: 500;
+  margin: 0;
+`;
+
+export const BodySmallSemiBold = styled.p`
+  font-size: 14px;
+  line-height: 22px;
+  font-weight: 600;
+  margin: 0;
+`;
+
+export const BodySmallBold = styled.p`
+  font-size: 14px;
+  line-height: 22px;
+  font-weight: 700;
+  margin: 0;
+`;
+
+export const BodySmallExtraBold = styled.p`
+  font-size: 14px;
+  line-height: 22px;
+  font-weight: 800;
+  margin: 0;
+`;
+
+// Body XSmall
+export const BodyXSmallRegular = styled.p`
+  font-size: 13px;
+  line-height: 18px;
+  font-weight: 400;
+  margin: 0;
+`;
+
+export const BodyXSmallMedium = styled.p`
+  font-size: 13px;
+  line-height: 18px;
+  font-weight: 500;
+  margin: 0;
+`;
+
+export const BodyXSmallSemiBold = styled.p`
+  font-size: 13px;
+  line-height: 18px;
+  font-weight: 600;
+  margin: 0;
+`;
+
+export const BodyXSmallBold = styled.p`
+  font-size: 13px;
+  line-height: 18px;
+  font-weight: 700;
+  margin: 0;
+`;
+
+export const BodyXSmallExtraBold = styled.p`
+  font-size: 13px;
+  line-height: 18px;
+  font-weight: 800;
+  margin: 0;
+`;
+
+// Body XXSmall
+export const BodyXXSmallRegular = styled.p`
+  font-size: 11px;
+  line-height: 16px;
+  font-weight: 400;
+  margin: 0;
+`;
+
+export const BodyXXSmallMedium = styled.p`
+  font-size: 11px;
+  line-height: 16px;
+  font-weight: 500;
+  margin: 0;
+`;
+
+export const BodyXXSmallSemiBold = styled.p`
+  font-size: 11px;
+  line-height: 16px;
+  font-weight: 600;
+  margin: 0;
+`;
+
+export const BodyXXSmallBold = styled.p`
+  font-size: 11px;
+  line-height: 16px;
+  font-weight: 700;
+  margin: 0;
+`;
+
+export const BodyXXSmallExtraBold = styled.p`
+  font-size: 11px;
+  line-height: 16px;
+  font-weight: 800;
+  margin: 0;
+`;
