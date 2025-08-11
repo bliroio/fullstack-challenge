785f94a2cfcdfea2814d74abfee95e7d0395c233
DedicatedUponMustard <DedicatedUponMustard@users.noreply.github.com>
2025-08-11 21:02:52 +0200

    feat: show error in frontend if end time is before start time

--- client/app/components/createMeeting.tsx
+++ client/app/components/createMeeting.tsx
@@ -1,5 +1,12 @@
-import { Box, Button, Drawer, TextField, Typography } from "@mui/material";
-import { FC, FormEvent } from "react";
+import {
+  Alert,
+  Box,
+  Button,
+  Drawer,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { FC, FormEvent, useState } from "react";
 import { createMeeting } from "../services/meetingService";
 
 interface Props {
@@ -8,6 +15,8 @@ interface Props {
 }
 
 const CreateMeeting: FC<Props> = ({ isOpen, onClose }) => {
+  const [formError, setFormError] = useState<string | null>(null);
+
   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
@@ -17,18 +26,28 @@ const CreateMeeting: FC<Props> = ({ isOpen, onClose }) => {
     const endTime = formData.get("endTime");
 
     if (!title || title === "" || !startTime || !endTime) {
+      setFormError("All fields are required.");
+      return;
+    }
+
+    const startTimeDate = new Date(startTime as string);
+    const endTimeDate = new Date(endTime as string);
+
+    if (startTimeDate >= endTimeDate) {
+      setFormError("Start time must be before end time.");
       return;
     }
 
     const data = {
       title: title as string,
-      startTime: new Date(startTime as string).toISOString(),
-      endTime: new Date(endTime as string).toISOString(),
+      startTime: startTimeDate.toISOString(),
+      endTime: endTimeDate.toISOString(),
     };
 
     createMeeting(data);
 
     onClose();
+    setFormError(null);
   };
 
   return (
@@ -40,6 +59,8 @@ const CreateMeeting: FC<Props> = ({ isOpen, onClose }) => {
           </Typography>
           <Button onClick={onClose}>Close</Button>
         </Box>
+
+        {formError && <Alert severity="error">{formError}</Alert>}
 
         <form onSubmit={onSubmit}>
           <TextField
