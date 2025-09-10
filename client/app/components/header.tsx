9f1126bc43ff492f4d316bfd7f2c7af78ce9e690
Martin Thoma <m.thoma@bliro.io>
2025-09-10 14:17:23 +0200

    chore: remove create meeting functionality

--- client/app/components/header.tsx
+++ client/app/components/header.tsx
@@ -1,44 +1,31 @@
-import { AppBar, Button, Drawer, Toolbar } from "@mui/material";
+import { AppBar, Toolbar } from "@mui/material";
 import { useState } from "react";
 import { Meeting } from "../models/Meeting";
-import { CreateMeetingModal } from "./create-meeting-modal";
-import createMeetingIcon from "./icons/arrow-up-right-square.svg";
 
 type Props = {
-    onCreateMeeting: (meeting: Omit<Meeting, "id">) => Promise<void>;
-}
+  onCreateMeeting: (meeting: Omit<Meeting, "id">) => Promise<void>;
+};
 export default function Header({ onCreateMeeting }: Props) {
-    const [drawerOpen, setDrawerOpen] = useState(false);
-    const handleCreateMeeting = async (meeting: Omit<Meeting, "id">) => {
-        try {
-            await onCreateMeeting(meeting);
-            setDrawerOpen(false);
-        } catch (error) {
-            console.error("Error creating meeting:", error);
-        }
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const handleCreateMeeting = async (meeting: Omit<Meeting, "id">) => {
+    try {
+      await onCreateMeeting(meeting);
+      setDrawerOpen(false);
+    } catch (error) {
+      console.error("Error creating meeting:", error);
     }
-    return (
-        <AppBar position="static">
-            <Toolbar sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-            }}>
-                <img
-                    src="/bliro_logo.svg"
-                    alt="Bliro Logo"
-                    style={{ height: 24 }}
-                />
-                <>
-                    <Button variant="contained" color="primary" onClick={() => setDrawerOpen(true)}>
-                        <img src={createMeetingIcon.src} alt="Create Meeting" style={{ height: 20, width: 20, marginRight: 8 }} />
-                        Create Meeting
-                    </Button>
-                    <Drawer anchor="right" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
-                        <CreateMeetingModal onClose={() => setDrawerOpen(false)} onCreateMeeting={handleCreateMeeting} />
-                    </Drawer>
-                </>
-            </Toolbar>
-        </AppBar>
-    );
+  };
+  return (
+    <AppBar position="static">
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <img src="/bliro_logo.svg" alt="Bliro Logo" style={{ height: 24 }} />
+      </Toolbar>
+    </AppBar>
+  );
 }
--- client/app/components/label.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-    children: React.ReactNode;
-    style?: React.CSSProperties;
-    required?: boolean;
-    htmlFor?: string;
-}
-export const Label = ({ children, style, htmlFor, required }: Props) => {
-    return <label htmlFor={htmlFor} style={{ fontWeight: 600, fontSize: '14px', lineHeight: '22px', color: '#2B313C', ...style }}>{children} {required && <span style={{ color: '#F43641' }}>*</span>}</label>
-}
